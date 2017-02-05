5f6410bc4c333360409a19a218b65db7af648809
Vladimir Roncevic <elektron.ronca@gmail.com>
2017-02-05 14:44:47 +0100

    Update logging mechanism && add template mechanism

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# gen_pl_pro
+Generating Perl Project Structure (Bash script).
+================================================================================
+
+The README is used to introduce the tool and provide instructions on
+how to install the tool, any machine dependencies it may have and any
+other information that should be provided before the tool is installed.
+
+INSTALLATION
+
+To install this tool type the following:
+
+	cp -R ~/gen_pl_pro/bin/   /root/scripts/gen_pl_pro/ver.1.0/
+
+	cp -R ~/gen_pl_pro/conf/  /root/scripts/gen_pl_pro/ver.1.0/
+
+	cp -R ~/gen_pl_pro/log/   /root/scripts/gen_pl_pro/ver.1.0/
+
+
+DEPENDENCIES
+
+This tool requires these other modules and libraries:
+
+	sh_util https://github.com/vroncevic/sh_util
+
+COPYRIGHT AND LICENCE
+
+Copyright (C) 2016 by https://github.com/vroncevic/gen_pl_pro
+
+This tool is free software; you can redistribute it and/or modify
+it under the same terms as Bash itself, either Bash version 4.2.47 or,
+at your option, any later version of Bash 4 you may have available.
+
+![alt tag](https://raw.githubusercontent.com/vroncevic/gen_pl_pro/master/bash_logo.png)
+![alt tag](https://raw.githubusercontent.com/vroncevic/gen_pl_pro/master/linux_logo.jpg)
+
---
+++ bin/gen_pl_pro.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+#
+# @brief   Generate Perl Tool Project
+# @version ver.1.0
+# @date    Thu Feb 07 00:46:32 2016
+# @company None, free software to use 2016
+# @author  Vladimir Roncevic <elektron.ronca@gmail.com>
+#
+UTIL_ROOT=/root/scripts
+UTIL_VERSION=ver.1.0
+UTIL=${UTIL_ROOT}/sh_util/${UTIL_VERSION}
+UTIL_LOG=${UTIL}/log
+
+.	${UTIL}/bin/devel.sh
+.	${UTIL}/bin/usage.sh
+.	${UTIL}/bin/check_root.sh
+.	${UTIL}/bin/check_tool.sh
+.	${UTIL}/bin/logging.sh
+.	${UTIL}/bin/send_mail.sh
+.	${UTIL}/bin/load_conf.sh
+.	${UTIL}/bin/load_util_conf.sh
+.	${UTIL}/bin/progress_bar.sh
+
+GEN_PL_PRO_TOOL=gen_pl_pro
+GEN_PL_PRO_VERSION=ver.1.0
+GEN_PL_PRO_HOME=${UTIL_ROOT}/${GEN_PL_PRO_TOOL}/${GEN_PL_PRO_VERSION}
+GEN_PL_PRO_CFG=${GEN_PL_PRO_HOME}/conf/${GEN_PL_PRO_TOOL}.cfg
+GEN_PL_PRO_UTIL_CFG=${GEN_PL_PRO_HOME}/conf/${GEN_PL_PRO_TOOL}_util.cfg
+GEN_PL_PRO_LOG=${GEN_PL_PRO_HOME}/log
+
+declare -A GENPLPRO_USAGE=(
+	[USAGE_TOOL]="${GEN_PL_PRO_TOOL}"
+	[USAGE_ARG1]="[PNAME] Perl App Project name"
+	[USAGE_EX_PRE]="# Generating Perl App project"
+	[USAGE_EX]="${GEN_PL_PRO_TOOL} RCP"
+)
+
+declare -A GEN_PL_PRO_LOGGING=(
+	[LOG_TOOL]="${GEN_PL_PRO_TOOL}"
+	[LOG_FLAG]="info"
+	[LOG_PATH]="${GEN_PL_PRO_LOG}"
+	[LOG_MSGE]="None"
+)
+
+declare -A PB_STRUCTURE=(
+	[BW]=50
+	[MP]=100
+	[SLEEP]=0.01
+)
+
+TOOL_DBG="false"
+TOOL_LOG="false"
+TOOL_NOTIFY="false"
+
+#
+# @brief   Main function 
+# @param   Value required project name
+# @retval  Function __gen_pl_pro exit with integer value
+#			0   - tool finished with success operation 
+#			128 - missing argument(s) from cli 
+#			129 - failed to load tool script configuration from file 
+#			130 - failed to load tool script utilities configuration from file
+#			131 - project already exist
+#
+# @usage
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# __gen_pl_pro "rtp"
+#
+function __gen_pl_pro() {
+	local PNAME=$1
+	if [ -n "${PNAME}" ]; then
+		local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
+		MSG="Loading basic and util configuration!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		__progress_bar PB_STRUCTURE
+		declare -A config_gen_pl_pro=()
+		__load_conf "$GEN_PL_PRO_CFG" config_gen_pl_pro
+		STATUS_CONF=$?
+		declare -A config_gen_pl_pro_util=()
+		__load_util_conf "$GEN_PL_PRO_UTIL_CFG" config_gen_pl_pro_util
+		STATUS_CONF_UTIL=$?
+		declare -A STATUS_STRUCTURE=(
+			[1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
+		)
+		__check_status STATUS_STRUCTURE
+		STATUS=$?
+		if [ $STATUS -eq $NOT_SUCCESS ]; then
+			MSG="Force exit!"
+			__info_debug_message_end "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+			exit 129
+		fi
+		TOOL_LOG=${config_gen_pl_pro[LOGGING]}
+		TOOL_DBG=${config_gen_pl_pro[DEBUGGING]}
+		TOOL_NOTIFY=${config_gen_pl_pro[EMAILING]}
+		MSG="Generating project structure [${PNAME}]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		GEN_PL_PRO_LOGGING[LOG_MSGE]="$MSG"
+		GEN_PL_PRO_LOGGING[LOG_FLAG]="info"
+		__logging GEN_PL_PRO_LOGGING
+		if [ -d "${PNAME}/" ]; then
+			MSG="Project structure already exist [${PNAME}]"
+			GEN_PL_PRO_LOGGING[LOG_MSGE]="$MSG"
+			GEN_PL_PRO_LOGGING[LOG_FLAG]="info"
+			__logging GEN_PL_PRO_LOGGING
+			MSG="Force exit!"
+			__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+			exit 131
+		fi
+		MSG="Generating directory [${PNAME}/]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		mkdir "${PNAME}/"
+		MSG="Generating directory [${PNAME}/bin/]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		mkdir "${PNAME}/bin/"
+		MSG="Generating file [${PNAME}/bin/${PNAME}.pl]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		local HASH="#" TAB="	" DATE=`date` BSLASH="\\" PL
+		local PT=${config_gen_pl_pro_util[PERL_TEMPLATE]}
+		local AUTHOR_NAME=${config_gen_pl_pro_util[AUTHOR_NAME]}
+		local AUTHOR_EMAIL=${config_gen_pl_pro_util[AUTHOR_EMAIL]}
+		local USER=${config_gen_pl_pro_util[USER]}
+		local GROUP=${config_gen_pl_pro_util[GROUP]}
+		local VERSION=${config_gen_pl_pro_util[VERSION]}
+		local PLF="${PNAME}/bin/${PNAME}.pl" PLT="${GEN_PL_PRO_HOME}/conf/${PT}"
+		while read PL
+		do
+			eval echo "${PL}" >> ${PLF}
+		done < ${PLT}
+		MSG="Generating directory [${PNAME}/conf/]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		mkdir "${PNAME}/conf/"
+		MSG="Generating file [${PNAME}/conf/${PNAME}.cfg]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		local CFG="${PNAME}/conf/${PNAME}.cfg" CL
+		local CT=${config_gen_pl_pro_util[CFG_TEMPLATE]}
+		local CFGT="${GEN_PL_PRO_HOME}/conf/${CT}"
+		while read CL
+		do
+			eval echo "${CL}" >> ${CFG}
+		done < ${CFGT}
+		MSG="Generating file [${PNAME}/conf/${PNAME}_util.cfg]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		local CFGU="${PNAME}/conf/${PNAME}_util.cfg"
+		local CTU=${config_gen_pl_pro_util[CFG_UTIL_TEMPLATE]}
+		local CFGTU="${GEN_PL_PRO_HOME}/conf/${CTU}"
+		while read CL
+		do
+			eval echo "${CL}" >> ${CFGU}
+		done < ${CFGTU}
+		MSG="Generating directory [${PNAME}/log/]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		mkdir "${PNAME}/log/"
+		MSG="Generating file [${PNAME}/log/${PNAME}.log]!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		touch "${PNAME}/log/${PNAME}.log"
+		MSG="Set owner!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		local USRID=${config_gen_pl_pro_util[UID]}
+		local GRPID=${config_gen_pl_pro_util[GID]}
+		eval "chown -R ${USRID}.${GRPID} ${PNAME}/"
+		MSG="Set permission!"
+		__info_debug_message "$MSG" "$FUNC" "$GEN_PL_PRO_TOOL"
+		eval "chmod -R 700 ${PNAME}/"
+		MSG="Generated project structure [${PNAME}]"
+		GEN_PL_PRO_LOGGING[LOG_MSGE]="$MSG"
+		GEN_PL_PRO_LOGGING[LOG_FLAG]="info"
+		__logging GEN_PL_PRO_LOGGING
+		__info_debug_message_end "Done" "$FUNC" "$GEN_PL_PRO_TOOL"
+		exit 0
+	fi
+	__usage GENPLPRO_USAGE
+	exit 128
+}
+
+#
+# @brief   Main entry point of script tool
+# @param   Value required project name
+# @exitval Script tool gen_pl_pro exit with integer value
+#			0   - tool finished with success operation 
+# 			127 - run tool script as root user from cli
+#			128 - missing argument(s) from cli 
+#			129 - failed to load tool script configuration from file 
+#			130 - failed to load tool script utilities configuration from file
+#			131 - project already exist
+#
+printf "\n%s\n%s\n\n" "${GEN_PL_PRO_TOOL} ${GEN_PL_PRO_VERSION}" "`date`"
+__check_root
+STATUS=$?
+if [ $STATUS -eq $SUCCESS ]; then
+	__gen_pl_pro $1
+fi
+
+exit 127
+
